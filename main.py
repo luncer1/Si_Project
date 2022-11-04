f9b3cb40afd62030e0042d5d0e16df076fda0599
Luncer <krisstoff123@gmail.com>
2022-11-04 22:33:32 +0100

    Finito

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import SGA
 
 if __name__ == '__main__':
-
+    pr_krzyz = 60
+    pr_mut = 5
+    l_os = 30
+    l_pop = int(150/l_os)
+    statistical_try = list()
     for i in range(40):
-        Generator = SGA.SGA(-1, 1, 0, 9, 70, 1)
+        Generator = SGA.SGA(11, 1, 6, l_os, pr_krzyz, pr_mut)
         populacja = Generator.wylosuj_pierwsza_populacje()
-        for i in range(15):
-          #  print(populacja,sum(populacja)/len(populacja)," przed")
+        for i in range(l_pop):
             wartosci_funkcji_celu = Generator.sprawdzenie_funkcji_celu(populacja)
             czy_ujemne = Generator.sprawdzenie_ujemnych(wartosci_funkcji_celu)
-            if czy_ujemne:
+            if czy_ujemne != False:
                 for key in wartosci_funkcji_celu.keys():
-                    wartosci_funkcji_celu[key] = wartosci_funkcji_celu[key] + (czy_ujemne * (-1) + 2)
-
+                    wartosci_funkcji_celu[key] = wartosci_funkcji_celu[key] + (czy_ujemne * (-1))
             wartosci_wkladu = Generator.wyliczenie_wkladu(wartosci_funkcji_celu)
             print("wartosci wkladu",wartosci_wkladu)
             populacja = Generator.selekcja(wartosci_wkladu)
@@ -31,3 +33,5 @@ if __name__ == '__main__':
         print("best osobnik",best_osobnik)
         results = open("results.txt", 'a')
         results.write(f"{Generator.funkcja_celu(best_osobnik)} {best_osobnik}\n")
+        statistical_try.append(best_osobnik)
+    print(sum(statistical_try)/len(statistical_try))
